435bab1b163497b35c8cda1127ff7993e513d6ba
MaksimDalgeda <maksim.dalgeda@gmail.com>
2026-03-18 15:16:51 +0200

    START_render_configuration

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
