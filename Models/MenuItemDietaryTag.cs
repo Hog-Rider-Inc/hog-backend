a65677d0e2eb0cb4295735cead32ee0e3ebb4aae
MaksimDalgeda <maksim.dalgeda@gmail.com>
2026-03-21 22:47:15 +0200

    HOG-31
    Prideta minimali klase: MenuItemDietaryTags

---
+++ Models/MenuItemDietaryTag.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HogRider.Backend.Models
+{
+    [Table("MenuItemDietaryTags")]
+    public class MenuItemDietaryTag
+    {
+        public int Id { get; set; }
+
+    }
+}
