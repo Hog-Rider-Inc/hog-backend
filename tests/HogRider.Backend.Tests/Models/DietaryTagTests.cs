e6164563544ea40d612994e7c36e3762a7c212bb
MaksimDalgeda <maksim.dalgeda@gmail.com>
2026-03-22 18:37:55 +0200

    HOG-30
    Unit tests

---
+++ tests/HogRider.Backend.Tests/Models/DietaryTagTests.cs
@@ -0,0 +1,30 @@
+﻿using HogRider.Backend.Data;
+using HogRider.Backend.Models;
+using HogRider.Backend.Tests.Data;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace HogRider.Backend.Tests.Models
+{
+    public class DietaryTagTests
+    {
+        [Fact]
+        public void Can_Create_DietaryTag()
+        {
+            using var context = TestDbContextFactory.Create();
+
+            var tag = new DietaryTag
+            {
+                Title = "Vegan",
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            context.DietaryTags.Add(tag);
+            context.SaveChanges();
+
+            Assert.Single(context.DietaryTags);
+        }
+    }
+}
---
+++ tests/HogRider.Backend.Tests/Relationships/MenuItemDietaryTagTests.cs
@@ -0,0 +1,49 @@
+﻿using HogRider.Backend.Data;
+using HogRider.Backend.Models;
+using HogRider.Backend.Tests.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogRider.Backend.Tests.Relationships
+{
+    public class MenuItemDietaryTagTests
+    {
+        [Fact]
+        public void Can_Link_MenuItem_DietaryTag()
+        {
+            using var context = TestDbContextFactory.Create();
+
+            var restaurant = TestDataFactory.CreateRestaurant(context);
+
+            var item = new MenuItem
+            {
+                Name = "Salad",
+                Price = 5,
+                RestaurantId = restaurant.Id,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            var tag = new DietaryTag { Title = "Vegan", CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow };
+
+            context.AddRange(item, tag);
+            context.SaveChanges();
+
+            var link = new MenuItemDietaryTag
+            {
+                MenuItemId = item.Id,
+                DietaryTagId = tag.Id,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            context.Add(link);
+            context.SaveChanges();
+
+            Assert.Single(context.MenuItemDietaryTags);
+        }
+    }
+}
