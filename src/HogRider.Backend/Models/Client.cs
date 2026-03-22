93457154a39ccf0853b65610c3ef4732d1287a80
MaksimDalgeda <maksim.dalgeda@gmail.com>
2026-03-22 16:04:54 +0200

    HOG-34
    Prideti modeliai : Client, Order

--- src/HogRider.Backend/Models/Client.cs
+++ src/HogRider.Backend/Models/Client.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HogRider.Backend.Models
 {
@@ -6,5 +7,34 @@ namespace HogRider.Backend.Models
     public class Client
     {
         public int Id { get; set; }
+
+        [Column("account_id")]
+        public int AccountId { get; set; }
+        public Account? Account { get; set; }
+
+        [Column("address_id")]
+        public int? AddressId { get; set; }
+        public Address? Address { get; set; }
+
+        [Column("first_name")]
+        public string? FirstName { get; set; }
+
+        [Column("last_name")]
+        public string? LastName { get; set; }
+
+        [Column("phone_number")]
+        public string? PhoneNumber { get; set; }
+
+        [Column("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        public List<Order> Orders { get; set; } = new();
+        public List<ClientFavourite> ClientFavourites { get; set; } = new();
+        public List<ClientItemInteraction> ClientItemInteractions { get; set; } = new();
+        public List<ClientRecommendation> ClientRecommendations { get; set; } = new();
+        public List<Review> Reviews { get; set; } = new();
     }
 }
