0c73ed03715deaf33a07f2421fcd69415a73b46e
MaksimDalgeda <maksim.dalgeda@gmail.com>
2026-03-22 17:53:24 +0200

    AppDbContext_update

--- src/HogRider.Backend/Data/AppDbContext.cs
+++ src/HogRider.Backend/Data/AppDbContext.cs
@@ -14,20 +14,24 @@ namespace HogRider.Backend.Data
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<Review> Reviews { get; set; }
         public DbSet<ClientFavourite> ClientFavourites { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<MenuItemCategory> MenuItemCategories { get; set; }
+        public DbSet<DietaryTag> DietaryTags { get; set; }
+        public DbSet<MenuItemDietaryTag> MenuItemDietaryTags { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
         // minimal stub'ai
-        public DbSet<Client> Clients { get; set; }
+
         public DbSet<Account> Accounts { get; set; }
         public DbSet<Address> Addresses { get; set; }
-        public DbSet<Order> Orders { get; set; }
         public DbSet<OrderMenuItem> OrderMenuItems { get; set; }
         public DbSet<MenuItemImage> MenuItemImages { get; set; }
         public DbSet<RestaurantLogoImage> RestaurantLogoImages { get; set; }
         public DbSet<ClientItemInteraction> ClientItemInteractions { get; set; }
         public DbSet<ClientRecommendation> ClientRecommendations { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<MenuItemCategory> MenuItemCategories { get; set; }
-        public DbSet<DietaryTag> DietaryTags { get; set; }
-        public DbSet<MenuItemDietaryTag> MenuItemDietaryTags {get; set;}
+       
+       
+       
     }
 }
