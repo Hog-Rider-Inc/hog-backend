6c4243063d2d66bbfe41cfbafeb1b966a61b634b
MaksimDalgeda <maksim.dalgeda@gmail.com>
2026-03-22 18:35:00 +0200

    HOG-28
    Unit Tests

---
+++ tests/HogRider.Backend.Tests/Models/CategoryTests.cs
@@ -0,0 +1,26 @@
+﻿using HogRider.Backend.Data;
+using HogRider.Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogRider.Backend.Tests.Models
+{
+    public class CategoryTests
+    {
+        [Fact]
+        public void Can_Create_Category()
+        {
+            using var context = TestDbContextFactory.Create();
+
+            var category = new Category { Title = "Fast Food", CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow };
+
+            context.Categories.Add(category);
+            context.SaveChanges();
+
+            Assert.Single(context.Categories);
+        }
+    }
+}
---
+++ tests/HogRider.Backend.Tests/Models/MenuItemCategoryTests.cs
@@ -0,0 +1,49 @@
+﻿using HogRider.Backend.Data;
+using HogRider.Backend.Models;
+using HogRider.Backend.Tests.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogRider.Backend.Tests.Models
+{
+    public class MenuItemCategoryTests
+    {
+        [Fact]
+        public void Can_Link_MenuItem_Category()
+        {
+            using var context = TestDbContextFactory.Create();
+
+            var restaurant = TestDataFactory.CreateRestaurant(context);
+
+            var item = new MenuItem
+            {
+                Name = "Burger",
+                Price = 8,
+                RestaurantId = restaurant.Id,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            var category = new Category { Title = "Fast Food", CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow };
+
+            context.AddRange(item, category);
+            context.SaveChanges();
+
+            var link = new MenuItemCategory
+            {
+                MenuItemId = item.Id,
+                CategoryId = category.Id,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            context.Add(link);
+            context.SaveChanges();
+
+            Assert.Single(context.MenuItemCategories);
+        }
+    }
+}
