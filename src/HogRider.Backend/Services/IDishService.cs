aee270ce5b9283ba32c17a7f7324f814a61c077c
MaksimDalgeda <maksim.dalgeda@gmail.com>
2026-03-29 01:43:00 +0200

    HOG-100 Pridetas Endpointas
    Pridetas endpointas : GET /api/dishes/{id}, kuris grazina informacija apie patiekala pagal ID
    
    https://hogriderproject.atlassian.net/browse/HOG-100

--- src/HogRider.Backend/Services/IDishService.cs
+++ src/HogRider.Backend/Services/IDishService.cs
@@ -5,5 +5,6 @@ namespace HogRider.Backend.Services
     public interface IDishService
     {
         Task<List<DishDto>> SearchAsync(string? q, string? category, string? dietary);
+        Task <DishDetailsDto?> GetDishByIdAsync(int id);
     }
 }
