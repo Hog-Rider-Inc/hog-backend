afcd4fc6d660153d0042b24e7203cac9144dc363
MaksimDalgeda <maksim.dalgeda@gmail.com>
2026-03-29 04:04:03 +0300

    HOG-115 Sukurtas endpointas GetOrders
    Sukurtas endpointas : /api/clients/{clientId}/orders
    
    https://hogriderproject.atlassian.net/browse/HOG-115

---
+++ src/HogRider.Backend/DTOs/OrdersDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HogRider.Backend.DTOs
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string Status { get; set; } = "";
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
